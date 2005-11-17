4ec46ed4afcebda7e439192eedfcaf804f8bdc0d
aquamaniac <aquamaniac@bfc04c16-8f10-0410-8200-ddfbc4d55d47>
2005-11-17 22:04:49 +0000

    added missing file
    git-svn-id: https://devel.aqbanking.de/svn/libchipcard/trunk@138 bfc04c16-8f10-0410-8200-ddfbc4d55d47

---
+++ src/lib/sharedstuff/misc.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ $RCSfile$
+                             -------------------
+    cvs         : $Id$
+    begin       : Mon Mar 01 2004
+    copyright   : (C) 2004 by Martin Preuss
+    email       : martin@libchipcard.de
+
+ ***************************************************************************
+ *          Please see toplevel file COPYING for license details           *
+ ***************************************************************************/
+
+
+#ifndef CHIPCARD2_MISC_H
+#define CHIPCARD2_MISC_H
+
+#include <chipcard2/chipcard2.h>
+
+#include <gwenhywfar/netlayer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
