6c8d60637026fc9ab43c1b70fd954a4310681720
martin <martin@bfc04c16-8f10-0410-8200-ddfbc4d55d47>
2008-08-18 15:00:30 +0000

    Debug script for starting chipcardd4 now uses /tmp for the PID file.
    git-svn-id: https://devel.aqbanking.de/svn/libchipcard/trunk@482 bfc04c16-8f10-0410-8200-ddfbc4d55d47

--- src/tools/chipcardd/runme.sh
+++ src/tools/chipcardd/runme.sh
@@ -2,4 +2,4 @@
 export LCDM_DRIVER_LOGLEVEL="info"
 export GWEN_LOGLEVEL="notice"
 #./chipcardd4 --logtype console --loglevel notice -f --pidfile chipcardd3.pid --store-all-certs
-./chipcardd4 --logtype console --loglevel info -f --pidfile chipcardd3.pid
+./chipcardd4 --logtype console --loglevel info -f --pidfile /tmp/chipcardd4.pid
