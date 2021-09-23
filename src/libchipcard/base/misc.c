b7f7e2e53cfc7f6903bd16b9bdccf0c1d3adf723
Martin Preuss <martin@aquamaniac.de>
2021-09-23 16:35:00 +0200

    Removed unused reader flags.

--- src/libchipcard/base/misc.c
+++ src/libchipcard/base/misc.c
@@ -32,24 +32,8 @@ uint32_t LC_ReaderFlags_fromDb(GWEN_DB_NODE *db, const char *name)
       break;
     if (strcasecmp(p, "keypad")==0)
       flags|=LC_READER_FLAGS_KEYPAD;
-    else if (strcasecmp(p, "display")==0)
-      flags|=LC_READER_FLAGS_DISPLAY;
-    else if (strcasecmp(p, "noinfo")==0)
-      flags|=LC_READER_FLAGS_NOINFO;
-    else if (strcasecmp(p, "remote")==0)
-      flags|=LC_READER_FLAGS_REMOTE;
-    else if (strcasecmp(p, "auto")==0)
-      flags|=LC_READER_FLAGS_AUTO;
-    else if (strcasecmp(p, "suspended_checks")==0)
-      flags|=LC_READER_FLAGS_SUSPENDED_CHECKS;
-    else if (strcasecmp(p, "driverHasVerify")==0)
-      flags|=LC_READER_FLAGS_DRIVER_HAS_VERIFY;
-    else if (strcasecmp(p, "keepRunning")==0)
-      flags|=LC_READER_FLAGS_KEEP_RUNNING;
     else if (strcasecmp(p, "lowWriteBoundary")==0)
       flags|=LC_READER_FLAGS_LOW_WRITE_BOUNDARY;
-    else if (strcasecmp(p, "noMemorySw")==0)
-      flags|=LC_READER_FLAGS_NO_MEMORY_SW;
     else {
       DBG_WARN(0, "Unknown flag \"%s\", ignoring", p);
     }
@@ -77,24 +61,8 @@ uint32_t LC_ReaderFlags_fromXml(GWEN_XMLNODE *node, const char *name)
 
       if (strcasecmp(p, "keypad")==0)
         flags|=LC_READER_FLAGS_KEYPAD;
-      else if (strcasecmp(p, "display")==0)
-        flags|=LC_READER_FLAGS_DISPLAY;
-      else if (strcasecmp(p, "noinfo")==0)
-        flags|=LC_READER_FLAGS_NOINFO;
-      else if (strcasecmp(p, "remote")==0)
-        flags|=LC_READER_FLAGS_REMOTE;
-      else if (strcasecmp(p, "auto")==0)
-        flags|=LC_READER_FLAGS_AUTO;
-      else if (strcasecmp(p, "suspended_checks")==0)
-        flags|=LC_READER_FLAGS_SUSPENDED_CHECKS;
-      else if (strcasecmp(p, "driverHasVerify")==0)
-        flags|=LC_READER_FLAGS_DRIVER_HAS_VERIFY;
-      else if (strcasecmp(p, "keepRunning")==0)
-        flags|=LC_READER_FLAGS_KEEP_RUNNING;
       else if (strcasecmp(p, "lowWriteBoundary")==0)
         flags|=LC_READER_FLAGS_LOW_WRITE_BOUNDARY;
-      else if (strcasecmp(p, "noMemorySw")==0)
-        flags|=LC_READER_FLAGS_NO_MEMORY_SW;
       else {
         DBG_WARN(0, "Unknown flag \"%s\", ignoring", p);
       }
@@ -107,41 +75,17 @@ uint32_t LC_ReaderFlags_fromXml(GWEN_XMLNODE *node, const char *name)
 
 
 
-void LC_ReaderFlags_toDb(GWEN_DB_NODE *db,
-                         const char *name,
-                         uint32_t fl)
+void LC_ReaderFlags_toDb(GWEN_DB_NODE *db, const char *name, uint32_t fl)
 {
   assert(db);
   assert(name);
   GWEN_DB_DeleteVar(db, name);
   if (fl & LC_READER_FLAGS_KEYPAD)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "keypad");
-  if (fl & LC_READER_FLAGS_DISPLAY)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "display");
-  if (fl & LC_READER_FLAGS_NOINFO)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "noinfo");
-  if (fl & LC_READER_FLAGS_REMOTE)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "remote");
-  if (fl & LC_READER_FLAGS_AUTO)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "auto");
-  if (fl & LC_READER_FLAGS_DRIVER_HAS_VERIFY)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "driverHasVerify");
-  if (fl & LC_READER_FLAGS_KEEP_RUNNING)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "keepRunning");
+    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT, name, "keypad");
   if (fl & LC_READER_FLAGS_LOW_WRITE_BOUNDARY)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "lowWriteBoundary");
-  if (fl & LC_READER_FLAGS_NO_MEMORY_SW)
-    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT,
-                         name, "noMemorySw");
+    GWEN_DB_SetCharValue(db, GWEN_DB_FLAGS_DEFAULT, name, "lowWriteBoundary");
 }
+
 
 
 
